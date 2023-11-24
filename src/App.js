9af586ee1cbda7f51ebc0b829e9fc856f780a8eb
joelinkaiyi <joelinkaiyi@gmail.com>
2023-11-24 13:01:07 +0800

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import ResponsiveAppBar from "./appBar";
+import Footer from "./Footer";
 import Selector from "./Selector";
 import Title from "./title";
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <ResponsiveAppBar />
       <Title />
       <Selector />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import { Box } from "@mui/material";
+
+export default function Footer() {
+  return (
+    <Box
+          sx={{
+          marginTop:'20.5rem',
+          padding: '2rem',
+          borderTop:'0.063rem solid #707070',  
+      }}
+      component="footer"
+    >
+      <Container maxWidth="sm">
+        <Typography variant="body2" color="#fff" align="center">
+          {"Copyright © "}
+          <Link color="inherit" href="https://your-website.com/">
+            Your Website
+          </Link>{" "}
+          {new Date().getFullYear()}
+          {"."}
+        </Typography>
+      </Container>
+    </Box>
+  );
+}
