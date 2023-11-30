88638c35bdb3dd049b747f5afe96ef24709cbcb2
joelinkaiyi <joelinkaiyi@gmail.com>
2023-11-30 14:47:22 +0800

    title change

--- src/title.js
+++ src/title.js
@@ -2,7 +2,7 @@ import React from 'react'
 const Title = () => {
     return ( 
         <div>
-            <h1 style={{color:'#fff',fontSize:'3rem'}}>進位轉換器</h1>
+            <h1 style={{color:'#fff',fontSize:'3rem'}}>進位計算機</h1>
         </div>
      );
 }
