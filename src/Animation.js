d6761dd7b5c927a70480611a104fd10aa093415b
joelinkaiyi <joelinkaiyi@gmail.com>
2023-12-07 18:27:42 +0800

    animation add

--- src/Animation.js
+++ src/Animation.js
@@ -1,17 +1,48 @@
-import React from 'react'
-const Animation = () => {
-    return ( 
-        <div>
-            <section className='animation-wrapper'>
-                <section className='animation'>
-                    <section className='hero'>
-                        <img src={require('./math.jpg')} alt='image' />
-                    </section>
-                </section>
-                <div className='slider'></div>
-            </section>
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import mathImage from './math.jpg';
+import './animation.css';
+const StartAnimation = ({ onAnimationComplete }) => {
+  useEffect(() => {
+  
+    const tl = gsap.timeline();
+
+    tl.fromTo(
+      ".hero img",
+      { opacity: 0, scale: 1.5 },
+      { opacity: 1, scale: 1, duration: 1.5 }
+    )
+      .fromTo(
+        ".slider",
+        { scaleX: 0 },
+        { scaleX: 1, transformOrigin: "left", duration: 1.5 },
+        "-=1.5"
+      )
+      .fromTo(
+        ".animation h1",
+        { y: -50, opacity: 0 },
+        { y: 0, opacity: 1, duration: 1 },
+        "-=0.5"
+      )
+      .eventCallback("onComplete", onAnimationComplete);
+    tl.play();
+
+  
+    return () => {
+      tl.kill(); 
+    };
+  }, [onAnimationComplete]);
+
+  return (
+    <section className="animation-wrapper">
+      <section className="animation">
+        <div className="hero">
+         <img src={mathImage} alt="animation" style={{width:'100%',height:'100%'}} />
         </div>
-     );
-}
- 
-export default Animation;
+        <div className="slider"></div>
+      </section>
+    </section>
+  );
+};
+
+export default StartAnimation;
--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import { useState } from "react";
 import About from "./About";
 import "./App.css";
 import ResponsiveAppBar from "./appBar";
 import Footer from "./Footer";
 import Selector from "./Selector";
 import Title from "./title";
+import StartAnimation from './Animation';
 function App() {
+   const [showAnimation, setShowAnimation] = useState(true);
+
+  const handleAnimationComplete = () => {
+    setShowAnimation(false);
+  };
   return (
     <div className="App">
-      <ResponsiveAppBar />
-      <Title />
-      <Selector />
-      <About />
-      <Footer />
+      {showAnimation ? (
+        <StartAnimation onAnimationComplete={handleAnimationComplete} />
+      ) : (
+        <div>
+          <ResponsiveAppBar />
+          <Title />
+          <Selector />
+          <About />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
